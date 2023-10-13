02518b2d1f729ae1a58e6a51240edacfb710ebf5
Philippe Miossec <pmiossec@gmail.com>
2023-10-13 12:29:07 +0200

    deploy: remove save branch after filter-branch

--- to_ghpages.sh
+++ to_ghpages.sh
@@ -3,3 +3,4 @@ git checkout -B gh-pages
 git add -f dist
 git commit -am "Rebuild website"
 git filter-branch -f --prune-empty --subdirectory-filter dist && git push -f origin gh-pages && git checkout master
+git update-ref -d refs/original/refs/heads/gh-pages
