926db48a625bca6fc834004c92d16c79875e05bf
Philippe Miossec <pmiossec@gmail.com>
2023-11-15 10:17:03 +0100

    Fetch all playlists

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,50 @@ const CLIENT_ID = "49db0fd1075941c8a76359263a5f2be0"
 const tokenKey = "spotify_perso_token";
 const tokenDateKey = tokenKey +"_date";
 
+interface PlaylistData {
+  title: string,
+  url: string
+}
+
 //Spotify authentication: https://dev.to/dom_the_dev/how-to-use-the-spotify-api-in-your-react-js-app-50pn
 const defaultPlaylist = "https://open.spotify.com/playlist/47dzOYBDRYt5ubbegyon3N"
 function App() {
   const [activePlaylistUrl, setActivePlaylistUrl] = useState(defaultPlaylist);
-  const [playlistUrls, setPlaylistUrls] = useState([{title: "perso", url: defaultPlaylist}]);
+  const [playlistUrls, setPlaylistUrls] = useState<PlaylistData[]>([{title: "perso", url: defaultPlaylist}]);
   const [token, setToken] = useState<string|null>(null);
 
-  const fetchPlaylists = async () => {
+  const fetchPlaylists = async (url: string, token: string) : Promise<PlaylistData[]>  => {
+    // TODO Fetch **all** playlist and cache them in local storage
+    // https://developer.spotify.com/documentation/web-api/reference/get-list-users-playlists
+    const response = await fetch(url,{
+      method: "GET",
+      headers: {
+        Authorization:`Bearer ${token}`
+      },
+    });
+
+    if (!response.ok) {
+      console.error("Call for playlists failed", response);
+      return [];
+    }
+
+    const data = await response.json();
+    const nextUrl = data.next as null | string;
+    const playlistData = data.items as [{ name: string, external_urls: {spotify: string }}];
+    // console.log("playlistData", playlistData);
+    const playLists: PlaylistData[] = playlistData.map(p => {
+      return { title: p.name, url: p.external_urls.spotify }
+    });
+
+    if (nextUrl) {
+      const nextPlaylists = await fetchPlaylists(nextUrl, token);
+      return [...playLists, ...nextPlaylists];
+    }
+
+    return playLists;
+  };
+
+  const fetchAllPlaylists = async () => {
 
     const hash = window.location.hash
     let extractedToken = window.localStorage.getItem(tokenKey)
@@ -42,33 +78,16 @@ function App() {
       return;
     }
 
-    // https://developer.spotify.com/documentation/web-api/reference/get-list-users-playlists
-    const response = await fetch(`https://api.spotify.com/v1/me/playlists`,{
-      method: "GET",
-      headers: {
-        Authorization:`Bearer ${extractedToken}`
-      },
-    });
-
-    if (!response.ok) {
-      console.error("Call for playlists failed", response);
-      return;
-    }
-
-    const data = await response.json();
-    const playlistData = data.items as [{ name: string, external_urls: {spotify: string }}];
-    // console.log("playlistData", playlistData);
-    const playLists = playlistData.map(p => {
-      return { title: p.name, url: p.external_urls.spotify }
-    }).sort((a, b)=> a.title.localeCompare(b.title));
-    console.log("playLists", playLists);
-    setPlaylistUrls(playLists);
+    const playLists = await fetchPlaylists(`https://api.spotify.com/v1/me/playlists?limit=50`, extractedToken)
+    const sortedPlaylists = playLists.sort((a, b)=> a.title.localeCompare(b.title));
+    console.log("playLists", sortedPlaylists);
+    setPlaylistUrls(sortedPlaylists);
     if (playLists.length === 1) {
       setActivePlaylistUrl(playLists[0].url);
     }
   };
 
-  useEffect(() => { fetchPlaylists(); }, []);
+  useEffect(() => { fetchAllPlaylists(); }, []);
 
   const logout = () => {
     setToken("");
